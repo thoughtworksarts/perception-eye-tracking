aec240b5670b28d1e315b1817d45684acdff719b
Dan Lewis-Toakley <dan.lewistoakley@gmail.com>
2019-07-23 18:04:20 -0400

    removed parser instaniation from callback function

--- eye_tracking/gaze_data_callback.py
+++ eye_tracking/gaze_data_callback.py
@@ -4,9 +4,8 @@ from eye_tracking.config import EYE_TRACKING_DATA_FILENAME
 
 
 def gaze_data_callback(gaze_data) -> None:
-    parser = Parser()
-    left_eye_coordinates = parser.get_coordinates_from_eye_data(gaze_data.left_eye)
-    right_eye_coordinates = parser.get_coordinates_from_eye_data(gaze_data.right_eye)
+    left_eye_coordinates = Parser.get_coordinates_from_eye_data(gaze_data.left_eye)
+    right_eye_coordinates = Parser.get_coordinates_from_eye_data(gaze_data.right_eye)
     local_storage = LocalStorage(file_name=EYE_TRACKING_DATA_FILENAME, mode='a+')
     local_storage.store(left_eye_coordinates, right_eye_coordinates)
     local_storage.close()
