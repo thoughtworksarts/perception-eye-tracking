f12c49496c0aaf8bee5143083acb824f38a3fc24
Dan Lewis-Toakley <dan.lewistoakley@gmail.com>
2019-07-23 20:34:05 -0400

    refactored eye tracker controller factory to improve api clarity

--- eye_tracking/api.py
+++ eye_tracking/api.py
@@ -3,9 +3,6 @@ import json
 from flask import Flask, Response
 
 from eye_tracking.gaze_data_callback import gaze_data_callback
-from eye_tracking.local_storage import LocalStorage
-from eye_tracking.config import EYE_TRACKING_DATA_FILENAME
-from eye_tracking.quadrant_calculator import QuadrantCalculator
 from eye_tracking.eye_tracker_controller_factory import EyeTrackerControllerFactory
 
 app = Flask(__name__)
@@ -13,7 +10,6 @@ app = Flask(__name__)
 
 @app.route('/start_eye_tracking')
 def start_eye_tracking():
-    # TODO: maybe add try/catch around this line
     eye_tracking_controller = EyeTrackerControllerFactory.get_eye_tracker_controller()
     eye_tracking_controller.subscribe_with_callback(gaze_data_callback)
     return Response(status=200, response='Success')
@@ -21,14 +17,9 @@ def start_eye_tracking():
 
 @app.route('/stop_eye_tracking')
 def stop_eye_tracking():
-    local_storage = LocalStorage(file_name=EYE_TRACKING_DATA_FILENAME, mode='r')
-    quadrant_calculator = QuadrantCalculator()
+    eye_tracking_controller = EyeTrackerControllerFactory.get_eye_tracker_controller()
 
-    eye_tracking_controller = EyeTrackerControllerFactory.get_eye_tracker_controller(
-        local_storage=local_storage, quadrant_calculator=quadrant_calculator
-    )
-
-    quadrant = quadrant_calculator.calculate_quadrant()
+    quadrant = eye_tracking_controller.get_quadrant()
     eye_tracking_controller.unsubscribe()
     return Response(status=200, response=json.dumps({'quadrant': quadrant}))
 
--- eye_tracking/eye_tracker_controller_factory.py
+++ eye_tracking/eye_tracker_controller_factory.py
@@ -6,17 +6,21 @@ from eye_tracking.eye_tracker_controller import EyeTrackerController
 from eye_tracking.config import DISPLAY_AREA_CONFIG
 from eye_tracking.local_storage import LocalStorage
 from eye_tracking.quadrant_calculator import QuadrantCalculator
+from eye_tracking.config import EYE_TRACKING_DATA_FILENAME
 
 
 class EyeTrackerControllerFactory:
 
     @staticmethod
-    def get_eye_tracker_controller(local_storage: LocalStorage, quadrant_calculator: QuadrantCalculator):
+    def get_eye_tracker_controller():
         eye_trackers = find_all_eyetrackers()
         eye_tracker = eye_trackers[0]
 
         display_area = DisplayArea(DISPLAY_AREA_CONFIG)
         eye_tracker.set_display_area(display_area)
+
+        local_storage = LocalStorage(file_name=EYE_TRACKING_DATA_FILENAME, mode='r')
+        quadrant_calculator = QuadrantCalculator()
 
         return EyeTrackerController(eye_tracker=eye_tracker, display_area=display_area,
                                     local_storage=local_storage, quadrant_calculator=quadrant_calculator)
