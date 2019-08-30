133a8f1ed1299ae55e43117df92a952f1d397775
Emilio Escobedo <emilio.escobedo@thoughtworks.com>
2019-08-30 15:02:23 -0400

    modified endpoints trying to follow restful architecture

--- eye_tracking/api.py
+++ eye_tracking/api.py
@@ -7,24 +7,23 @@ from eye_tracking.eye_tracker_controller_factory import EyeTrackerControllerFact
 app = Flask(__name__)
 
 
-@app.route('/start_eye_tracking')
+@app.route('/eye-tracking/start', methods=['POST']) # levelId, startTime, stopTime
 def start_eye_tracking():
     eye_tracking_controller = EyeTrackerControllerFactory.get_eye_tracker_controller(mode='a+')
-    eye_tracking_controller.subscribe_with_callback(request.args['levelId'])
+    eye_tracking_controller.subscribe_with_callback(request.form['levelId'])
     return Response(status=200, response='Success')
 
 
-@app.route('/stop_eye_tracking')
+@app.route('/eye-tracking/stop', methods=['POST']) # levelId
 def stop_eye_tracking():
     eye_tracking_controller = EyeTrackerControllerFactory.get_eye_tracker_controller(mode='r')
     eye_tracking_controller.unsubscribe()
     return Response(status=200, response='Success')
 
-@app.route('/create_eye_tracking_visualization')
+@app.route('/eye-tracking/visualization', methods=['POST'])
 def create_eye_tracking_visualization():
     eye_tracking_controller = EyeTrackerControllerFactory.get_eye_tracker_controller(mode='r')
     return eye_tracking_controller.create_eye_tracking_visualization(request.args['levels'])
-
 
 if __name__ == '__main__':
     app.run(debug=True)
